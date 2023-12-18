82d98f9c2cb82660d360be336c8ad9618c3af85b
Henry <henry.bogita@student.moringaschool.com>
2023-12-18 13:28:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-// index.js
-
 function superbowlWin(record) {
-    const winObject = record.find(entry => entry.result === 'W');
-    
-    // If there is a win object, return the corresponding year; otherwise, return undefined.
-    return winObject ? winObject.year : undefined;
+    const win = record.find(obj => obj.result === "W");
+  
+    if (win) {
+      return win.year;
+    } else {
+      return undefined;  
+    }
   }
-  
-  // Example usage:
-  const record = [
-    { year: "2015", result: "W"},
-    { year: "2014", result: "N/A"},
-    { year: "2013", result: "L"},
-    // ... other entries
-  ];
-  
-  const winningYear = superbowlWin(record);
-  console.log(winningYear); // This will print the year of the Super Bowl win or undefined if not found
-  
